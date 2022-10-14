864fbcb3faaddd96d372e001bebf4671e8e3d14d
williambjork <william.bjork@gobil.se>
2022-10-14 10:29:31 +0200

    google auth implemented

--- pages/auth/login.js
+++ pages/auth/login.js
@@ -1,10 +1,29 @@
+import { FcGoogle } from 'react-icons/fc'
+import { signInWithPopup, GoogleAuthProvider } from 'firebase/auth'
+import { auth } from "../../utils/firebase"
+import { Router } from 'next/router';
+
 function login() {
+
+  //Sign in with google
+const googleProvider = new GoogleAuthProvider();
+const GoogleLogin = async () => {
+  try {
+    const result = await signInWithPopup(auth, googleProvider);
+    Router.push("/")
+  } catch (error) {
+
+  }
+}
+
   return (
     <div className="shadow-xl mt-32 p-10 text-gray-700 rounded-lg">
       <h2 className="text-2xl font-medium">Join Today</h2>
       <div className="py-4">
         <h3 className="py-4">Sign in with one of the providers</h3>
-        <button className="rounded-lg flex align-middle p-4 w-full text-white bg-gray-700">Sign in with Google</button>
+        <button onClick={GoogleLogin} className="rounded-lg flex align-middle gap-2 p-4 w-full text-white bg-gray-700">
+        <FcGoogle className='text-2xl'/> Sign in with Google</button>
+        
       </div>
     </div>
   );
--- utils/firebase.js
+++ utils/firebase.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth } from 'firebase';
-import { getFireStore } from 'firebase/firestore'
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore'
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -19,5 +19,5 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth();
-export const db = getFireStore(app);
+export const db = getFirestore(app);
 
