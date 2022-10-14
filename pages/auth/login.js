dbc9b48688f4a5d1d51000e9634875e9d35753f8
williambjork <william.bjork@gobil.se>
2022-10-14 09:34:11 +0200

    styling

---
+++ pages/auth/login.js
@@ -0,0 +1,13 @@
+function login() {
+  return (
+    <div className="shadow-xl mt-32 p-10 text-gray-700 rounded-lg">
+      <h2 className="text-2xl font-medium">Join Today</h2>
+      <div className="py-4">
+        <h3 className="py-4">Sign in with one of the providers</h3>
+        <button className="rounded-lg flex align-middle p-4 w-full text-white bg-gray-700">Sign in with Google</button>
+      </div>
+    </div>
+  );
+}
+
+export default login;
