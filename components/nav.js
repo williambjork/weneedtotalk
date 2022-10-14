f8d0bae9c58495754e70b2528aebad2b4fa66b96
williambjork <william.bjork@gobil.se>
2022-10-14 10:43:05 +0200

    added post and dashboard button, styling

--- components/nav.js
+++ components/nav.js
@@ -1,7 +1,11 @@
 import Link from "next/link"
+import { auth } from "../utils/firebase"
+import { useAuthState } from 'react-firebase-hooks/auth'
 
 
 function Nav() {
+const [user, loading] = useAuthState(auth);
+
   return (
     
         <nav className="flex justify-between items-ccenter py-10">
@@ -11,12 +15,26 @@ function Nav() {
         
 
         <ul className="flex items-center gap-10">
+
+            
+            {!user && (
             <Link href={"/auth/login"}>
                 <a className="py-2 px-4 text-sm bg-cyan-500 text-white rounded-lg font-medium ml-8">
                     Join Now
                 </a>
             </Link>
+            )}
 
+            {user && (
+                <div className="flex items-center gap-6">
+                    <Link href="/post">
+                        <button className="font-medium bg-cyan-500 text-white py-2 px-4 rounded-mg text-small">Post</button>
+                    </Link>
+                    <Link href="/dashboard">
+                        <h1>Dashboard</h1>
+                    </Link>
+                </div>
+            )}
         </ul>
         </nav>
     
--- pages/auth/login.js
+++ pages/auth/login.js
@@ -1,20 +1,34 @@
 import { FcGoogle } from 'react-icons/fc'
 import { signInWithPopup, GoogleAuthProvider } from 'firebase/auth'
 import { auth } from "../../utils/firebase"
-import { Router } from 'next/router';
+import { useRouter } from 'next/router';
+import { useAuthState } from 'react-firebase-hooks/auth'
+import { useEffect } from 'react'
 
 function login() {
+
+  const route = useRouter();
+  const [user, loading] = useAuthState(auth);
 
   //Sign in with google
 const googleProvider = new GoogleAuthProvider();
 const GoogleLogin = async () => {
   try {
     const result = await signInWithPopup(auth, googleProvider);
-    Router.push("/")
+    route.push("/")
   } catch (error) {
 
   }
 }
+
+//If user logged in redirect to home
+useEffect(() => {
+  if(user){
+    route.push("/")
+  } else {
+    console.log("login")
+  }
+}, [user]);
 
   return (
     <div className="shadow-xl mt-32 p-10 text-gray-700 rounded-lg">
